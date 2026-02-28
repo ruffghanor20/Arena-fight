8dd27ed1001ed05d15690bcb4163042f1c6b2818
ruffghanor20 <133601363+ruffghanor20@users.noreply.github.com>
2026-02-28 15:02:01 -0300

    Delete boss-fight-gatsby-starter directory

--- boss-fight-gatsby-starter/gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "Boss Fight",
-    description: "Projeto de batalha contra chefes com progressão, loja, skills e evolução.",
-    author: "ruffghanor20"
-  },
-  pathPrefix: "/Boss-fight"
-};
